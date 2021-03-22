a6059e465039fce1c173e825555a3bbe596d115d
miles <moglendenning@gmail.com>
2021-03-22 10:32:56 -0500

    first of book 2: very simple! did you know that a cut up rectange equals a non cut up rectangle?

---
+++ proposition 02.01.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+screen = pygame.display.set_mode((600, 600))
+
+screen.fill((255, 255, 255))
+
+# Parts of cutting up a rectangle equal the whole of the rectangle
+
+a = (100, 200)
+b = (350, 200)
+c = (500, 200)
+d = (100, 400)
+e = (350, 400)
+f = (500, 400)
+
+pygame.draw.line(screen, (0, 0, 0), a, c, 2)
+pygame.draw.line(screen, (0, 0, 0), c, f, 2)
+pygame.draw.line(screen, (0, 0, 0), f, d, 2)
+pygame.draw.line(screen, (0, 0, 0), d, a, 2)
+pygame.draw.line(screen, (0, 0, 0), e, b, 2)
+
+pygame.display.flip()
+
+input()
