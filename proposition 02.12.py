89190d3b70407ed5dcdeb6900e8c893167a473b9
miles <moglendenning@gmail.com>
2021-04-13 11:05:03 -0500

    crappy job wanna play with fren don tell pls thx

---
+++ proposition 02.12.py
@@ -0,0 +1,31 @@
+import pygame, sys, math
+from pygame.locals import *
+from euclidMath import Math
+pygame.init()
+
+Math = Math()
+
+screen = pygame.display.set_mode((600, 600))
+
+screen.fill((255, 255, 255))
+
+# Parts of a whole add up
+
+a = (200, 400)
+b = (400, 400)
+c = (200, 350)
+
+d = (300, 400)
+
+pygame.draw.line(screen, (0, 0, 0), a, b, 2)
+pygame.draw.line(screen, (0, 0, 0), c, b, 2)
+pygame.draw.line(screen, (0, 0, 0), a, c, 2)
+
+pygame.draw.line(screen, (0, 0, 0), d, c, 2)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+
