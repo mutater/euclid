8bed3438c7ddfc1665cf540949f7bcd56ea0f5b9
miles <moglendenning@gmail.com>
2021-04-22 12:03:38 -0500

    dad is very gay, no more dad!!!

---
+++ proposition 03.03.py
@@ -0,0 +1,30 @@
+import pygame, sys, math
+from pygame.locals import *
+from euclidMath import Math
+pygame.init()
+
+Math = Math()
+
+screen = pygame.display.set_mode((600, 600))
+
+screen.fill((255, 255, 255))
+
+# If a straight line through a circle bisects a line, then the angles where the line bisects are right, and vice versa.
+
+a = (250, 350)
+b = (350, 350)
+
+c = (300, 300)
+distCB = Math.distance(a[0], c[0], a[1], c[1])
+
+pygame.draw.line(screen, (0, 0, 0), a, b, 2)
+pygame.draw.line(screen, (0, 0, 0), (300, 600), (300, 0), 2)
+
+pygame.draw.circle(screen, (0, 0, 0), c, distCB, 2)
+
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
