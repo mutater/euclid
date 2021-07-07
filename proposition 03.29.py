b83fad124f91852f1a18c7391b761e6cfa4590e2
miles <moglendenning@gmail.com>
2021-07-07 10:43:10 -0500

    apparently forgot to git the last prop?

---
+++ proposition 03.29.py
@@ -0,0 +1,40 @@
+import pygame, sys, math
+from pygame.locals import *
+from euclidMath import Math
+pygame.init()
+
+Math = Math()
+
+screen = pygame.display.set_mode((600, 600))
+
+screen.fill((255, 255, 255))
+
+#In equal circles equal circumfrences are subtended by equal straight lines
+
+a = (200, 300)
+c = (150, 380)
+d = (250, 380)
+
+e = 100
+
+f = (500, 300)
+h = (450, 380)
+i = (550, 380)
+
+pygame.draw.line(screen, (0, 0, 0), d, c, 2)
+pygame.draw.line(screen, (0, 0, 0), a, c, 2)
+pygame.draw.line(screen, (0, 0, 0), a, d, 2)
+
+pygame.draw.circle(screen, (0, 0, 0), a, e, 2)
+
+pygame.draw.line(screen, (0, 0, 0), f, h, 2)
+pygame.draw.line(screen, (0, 0, 0), i, h, 2)
+pygame.draw.line(screen, (0, 0, 0), i, f, 2)
+
+pygame.draw.circle(screen, (0, 0, 0), f, e, 2)
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
